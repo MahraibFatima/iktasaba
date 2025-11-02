92e1f2e74fdf637e377095c07f5b21d3fec67c51
MahraibFatima <mahraibfatima5@gmail.com>
2025-11-02 21:19:41 +0500

    update the js for accordion and css

--- React/singleAccordian/src/App.jsx
+++ React/singleAccordian/src/App.jsx
@@ -1,18 +1,57 @@
 import './App.css'
 import { faqData } from './data.js'
-function App() {
-  return (
-    <>
-      {
-        faqData.map(item => (
-          <div key={item.id}>
-            <h2>{item.question}</h2>
-            <p>{item.answer}</p>
-          </div>
-        ))}
+import { useState } from 'react'
 
-      <h1>Hi React!</h1>
-    </>
+function App() {
+  const [openItem, setOpenItem]= useState(null)
+
+  const toggleItem=(id)=>{
+    setOpenItem(openItem===id? null:id)
+  }
+const handleKeyDown=(e,id)=>{
+  if(e.key==='Enter' || e.key===' '){
+    e.preventDefault()
+    toggleItem(id)
+  }
+}
+
+  return (
+    <div className="app">
+      <div className='card'>
+        <h1 className="title"> FAQs</h1>
+        <div className= "accordion">
+          {
+            faqData.map(item=>{
+              const isOpen= openItem=== item.id
+              return(
+                <div key={item.id} className="item">
+                  <button className="trigger"
+                    onClick={()=>toggleItem(item.id)}
+                    onKeyDown={(e)=> handleKeyDown(e,item.id)}
+                    aria-expanded={isOpen}
+                    aria-controls= {`panel-${item.id}`}
+                  >
+                    <span>{item.question}</span>
+                    <span className={`chev ${isOpen?'open':''}`}>
+                      </span>
+                  </button>
+
+                  <div
+                    id={`panel-${item.id}`}
+                    className="panel"
+                    hidden={!isOpen}
+                    role="region"
+                  >
+                    <p>{item.answer}</p>
+                    </div>
+                </div>
+
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
